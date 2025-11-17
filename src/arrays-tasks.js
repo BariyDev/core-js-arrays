090aa900bc8a0a1616675d121c31bc617cf96a5a
BariyDev <barinov2702@gmail.com>
2025-11-18 02:51:36 +0300

    feat: implement getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -416,8 +416,13 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((number, index) => {
+      return { number, index };
+    })
+    .filter((item) => item.number % 2 !== 0)
+    .map((item) => item.index);
 }
 
 /**
